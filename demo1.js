4f7c547f3d2c8c6041a95bf69fce161df366a0db
dustinpfister <dustin.pfister@gmail.com>
2019-09-23 13:04:59 -0400

    basic idea togeather

---
+++ demo1.js
@@ -0,0 +1,1 @@
+let genFile = require('./index.js')
--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 let fs = require('fs'),
 util = require('util'),
-writeFile = util.promisify(fs.writeFile);
+path = require('path'),
+writeFile = util.promisify(fs.writeFile),
+mkdirp = util.promisify(require('mkdirp'));
 
 // the genFile method
-module.exports = (path, data) => {
-	
-	
-	
+module.exports = (path, data, options) => {
+
+    // make path absolute
+    path = path.resolve(path);
+
+    // using given options or these defaults
+    options = options || {
+        encoding: 'utf8',
+        mode: 0o666,
+        flag: 'w'
+    }
+
+    // mkdirp the path
+    return mkdirp(path)
+
+    // then write the data to the file
+    .then(() => {
+        writeFile(data, options);
+    });
+
 };
