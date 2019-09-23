dda171378acedcd3fc1cb335562be4625d73c12c
dustinpfister <dustin.pfister@gmail.com>
2019-09-23 13:43:57 -0400

    forGen

--- index.js
+++ index.js
@@ -5,38 +5,37 @@ writeFile = util.promisify(fs.writeFile),
 mkdirp = util.promisify(require('mkdirp'));
 
 // the genFile method
-let genFile = (filePath, data, options) => {
-
+let genFile = (filePath, data, options, forGen) => {
     // make path absolute
     filePath = path.resolve(filePath);
-
     // using given options or these defaults
     options = options || {
         encoding: 'utf8',
         mode: 0o666,
         flag: 'w'
-    }
-
+    };
+    forGen = forGen === undefined ? (filePath, data) => {
+        console.log('\u001b[32m' + 'genFile: ' + filePath + '\u001b[39m');
+    }: forGen;
     // mkdirp the path to the file
     return mkdirp(path.dirname(filePath))
-
     // then write the data to the file
     .then(() => {
         return writeFile(filePath, data, options);
     })
     .then(() => {
-        console.log('\u001b[32m'+ 'genFile: ' + filePath + '\u001b[39m');
-    })
 
+        forGen(filePath, data)
+
+    })
 };
 
 // the main method
-module.exports = (fileObjects, options) => {
-
+module.exports = (fileObjects, options, forGen) => {
     if (Array.isArray(fileObjects)) {
         return Promise.all(fileObjects.map((fileObj) => {
-                return genFile(fileObj.path, fileObj.data, options);
+                return genFile(fileObj.path, fileObj.data, options, forGen);
             }));
     }
-    return genFile(fileObjects.path, fileObjects.data, options)
-}
+    return genFile(fileObjects.path, fileObjects.data, options, forGen)
+};
